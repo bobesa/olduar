2ca1ea4fa50491f9ec4e659491dc3c35bb41f031
Vlastimil Senfeld <bobesa@gammosaur.com>
2014-04-25 19:40:02 +0200

    disabled tests

--- server/src/olduar/tests.go
+++ server/src/olduar/tests.go
@@ -5,7 +5,7 @@ import (
 )
 
 func RunTests() {
-	//return //Comment/Remove this to run tests
+	return //Comment/Remove this to run tests
 	fmt.Println("\nRunning tests")
 	testResult("Combat",testCombat())
 	fmt.Println()
