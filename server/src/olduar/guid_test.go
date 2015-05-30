54975708f46cbe2cc9db48386aa1639f18b98388
Vlastimil Senfeld <bobesa@gammosaur.com>
2015-05-30 17:15:52 +0200

    ++guid test

---
+++ server/src/olduar/guid_test.go
@@ -0,0 +1,16 @@
+package olduar
+
+import "testing"
+
+func TestGuids(t *testing.T) {
+	tests := make(map[GUID]GUID)
+	for i := 0; i < 500; i++ {
+		guid := GenerateGUID()
+		if _, exists := tests[guid]; exists {
+			t.Error("Duplicate GUID found")
+			return
+		} else {
+			tests[guid] = guid;
+		}
+	}
+}
